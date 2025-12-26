7120189896c4ab8936fbaa26800bd38fd53f1032
Nisk36 <81093941+Nisk36@users.noreply.github.com>
2025-12-26 15:36:29 +0900

    Set document language to Japanese

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
